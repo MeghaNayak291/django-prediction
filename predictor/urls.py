4564de255ffbb251ec9c38091d3c7bc3ceaf5a4d
Meghana <meghanayakmeghanayak90@gmail.com>
2026-02-26 10:28:22 +0530

    gbugh

--- predictor/urls.py
+++ predictor/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from .views import predict_api, home, history_view, decor_view
 
 urlpatterns = [
-    path('', decor_view, name='home'),
+    path('', decor_view, name='landing'),
+    path('detect/', home, name='home'),
     path('api/predict/', predict_api),
     path('history/', history_view, name='history'),
 ]
